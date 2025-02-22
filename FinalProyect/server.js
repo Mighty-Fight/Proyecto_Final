187af6fd82eebd0503ead2c24b75ada6c8580a88
Mighty-Fight <amantillac@uninorte.edu.co>
2025-02-22 18:19:38 -0500

    modified:   FinalProyect/server.js

--- FinalProyect/server.js
+++ FinalProyect/server.js
@@ -146,7 +146,6 @@ app.get('/search_plates', (req, res) => {
 });
 
 // Iniciar el servidor
-server.listen(3000, '0.0.0.0', () => {
-    console.log('Servidor corriendo en http://0.0.0.0:80');
+server.listen(80, () => {
+    console.log('Servidor web corriendo en http://localhost:80');
 });
-
