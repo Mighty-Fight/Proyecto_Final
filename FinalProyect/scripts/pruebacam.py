a1fa9a59f871091911d1566ba92eae0a29227f67
Mighty-Fight <amantillac@uninorte.edu.co>
2025-03-18 14:39:25 -0500

    Cambio de login mensaje && ya

--- FinalProyect/scripts/pruebacam.py
+++ FinalProyect/scripts/pruebacam.py
@@ -1,27 +1,11 @@
 import cv2
 
 rtsp_url = "rtsp://admin:abcd1234..@186.119.52.250:554/Streaming/Channels/101"
-
-cap = cv2.VideoCapture(rtsp_url, cv2.CAP_FFMPEG)
-cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"H264"))  # Fuerza H.264
-
-
+cap = cv2.VideoCapture(rtsp_url)
 
 if not cap.isOpened():
-    print("❌ No se  pudo conectar a la cámara. Verifica la URL RTSP.")
+    print("❌ No se pudo conectar a la cámara. Verifica la URL RTSP o la conexión de red.")
 else:
     print("✅ Conexión a la cámara establecida.")
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("⚠️ No se pudo obtener frame. Intentando nuevamente...")
-        break
-
-    cv2.imshow("Transmisión RTSP", frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
 cap.release()
-cv2.destroyAllWindows()
