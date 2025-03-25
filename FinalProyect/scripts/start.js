e705613157ac6cafe8d99a9aaa127c86c40f124c
Aronnam117 <mateoaronna@hotmail.com>
2025-03-24 19:52:40 -0500

    besito chao camnew

--- FinalProyect/scripts/start.js
+++ FinalProyect/scripts/start.js
@@ -13,8 +13,8 @@ serverProcess.stdout.on("data", (data) => console.log(`SERVER: ${data}`));
 serverProcess.stderr.on("data", (data) => console.error(`SERVER ERROR: ${data}`));
 
 // Iniciar el script de Python (camnew.py)
-console.log("Iniciando el reconocimiento de placas con camnew.py...");
-const camnewProcess = exec(`python3 ${camnewPath}`); // Usa 'python' en lugar de 'py' para compatibilidad
+//console.log("Iniciando el reconocimiento de placas con camnew.py...");
+//const camnewProcess = exec(`python3 ${camnewPath}`); // Usa 'python' en lugar de 'py' para compatibilidad
 
-camnewProcess.stdout.on("data", (data) => console.log(`CAMNEW: ${data}`));
-camnewProcess.stderr.on("data", (data) => console.error(`CAMNEW ERROR: ${data}`));
+//camnewProcess.stdout.on("data", (data) => console.log(`CAMNEW: ${data}`));
+//camnewProcess.stderr.on("data", (data) => console.error(`CAMNEW ERROR: ${data}`));
