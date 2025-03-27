f2984428a18a46be1ca8477d446a27af9758549e
gabrielcharris <gabrielrodriguezcharris@gmail.com>
2025-03-27 07:47:03 -0500

    update de la trasmisión

--- FinalProyect/scripts/videofeed.py
+++ FinalProyect/scripts/videofeed.py
@@ -25,10 +25,10 @@ def generate_frames():
         yield (b"--frame\r\n"
                b"Content-Type: image/jpeg\r\n\r\n" + frame_bytes + b"\r\n")
 
-@app.route("/video_feed")
+@app.route("/trasmisión")
 def video_feed():
     return Response(generate_frames(),
                     mimetype="multipart/x-mixed-replace; boundary=frame")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002, debug=False)
+    app.run(host="0.0.0.0", port=5001, debug=False)
