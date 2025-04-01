bb226199da945e5cb780445e94f683e64362be68
Aronnam117 <tu-correo@gmail.com>
2025-04-01 09:20:23 -0500

    cambio de IP de camara

--- FinalProyect/scripts/camnew.py
+++ FinalProyect/scripts/camnew.py
@@ -16,7 +16,7 @@ import atexit
 import base64
 
 # === CONFIGURACIÓN (vía variables de entorno o valores por defecto) ===
-RTSP_URL = os.getenv("RTSP_URL", "rtsp://admin:abcd1234..@181.236.149.17:554/Streaming/Channels/101")
+RTSP_URL = os.getenv("RTSP_URL", "rtsp://admin:abcd1234..@181.236.138.128:554/Streaming/Channels/101")
 
 # ROI donde esperamos encontrar la placa. Ajusta estas coordenadas:
 ROI_COORDS = (520, 120, 400, 400)
