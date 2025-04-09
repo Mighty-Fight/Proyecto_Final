11694f6c57686edcbf29d1ca16b928b0a61091db
Mighty-Fight <amantillac@uninorte.edu.co>
2025-04-09 02:17:10 -0500

    Hola

--- FinalProyect/server.js
+++ FinalProyect/server.js
@@ -276,5 +276,5 @@ app.post('/submit-feedback', (req, res) => {
 
 const PORT = process.env.PORT || 80;
 server.listen(PORT, () => {
-    console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`✅ Servidor corriendoo en http://localhost:${PORT}`);
 });
